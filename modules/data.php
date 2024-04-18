2cf8e10fe4140da5508eb07d930aa89430351c12
Alejandro Vives <alejandrovt1996@gmail.com>
2024-04-18 16:33:38 +0200

    colors

--- modules/data.php
+++ modules/data.php
@@ -1,1 +1,26 @@
+<?php
+    require_once '../includes/connection.php';
+    // Verificar si el empleado está autenticado
+    if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
+        header("Location: ../index.php");
+        exit();
+    }
+?>
+
+<?php include_once("../includes/header-admin.php"); ?>
+
+
+<main class="main-employees">
+        <h1>Mis datos</h1>
+        <a href="edit-data.php" class="btn btn-edit-data">Editar mis datos</a>
+        <div class="container">
+            <table class="employee-table">
+
+
+
+            </table>
+        </div>
+    </main>
+
+<?php include_once("../includes/footer.php"); ?>
 
--- modules/employees.php
+++ modules/employees.php
@@ -17,75 +17,77 @@
 
     <link rel="stylesheet" href="../assets/css/style-main.css"> 
 
-    <main class="main-employees">
-        <h1>Lista de empleados</h1>
-        <a href="add-employee.php" class="btn add-employee-btn">Añadir empleado</a>
-        <div class="container">
-            <table class="employee-table">
+    <div class="employees-container">
+        <main class="main-employees">
+            <h1>Lista de empleados</h1>
+            <a href="add-employee.php" class="btn add-employee-btn">Añadir empleado</a>
+            <div class="container">
+                <table class="employee-table">
 
-                <?php
-                    require_once '../includes/connection.php';
+                    <?php
+                        require_once '../includes/connection.php';
 
-                // Obtener empleados
-                $sql = "SELECT * FROM empleados";
-                $result = mysqli_query($db, $sql);
+                    // Obtener empleados
+                    $sql = "SELECT * FROM empleados";
+                    $result = mysqli_query($db, $sql);
 
-                // Verificar si se encontraron empleados
-                if ($result && mysqli_num_rows($result) > 0) {
+                    // Verificar si se encontraron empleados
+                    if ($result && mysqli_num_rows($result) > 0) {
 
-                    echo '<table class="employee-table">';
-                        echo '<thead>';
+                        echo '<table class="employee-table">';
+                            echo '<thead>';
+                                echo '<tr>';
+                                    echo '<th>Foto</th>';
+                                    echo '<th>DNI</th>';
+                                    echo '<th>Nombre</th>';
+                                    echo '<th>Apellidos</th>';
+                                    echo '<th>Email</th>';
+                                    echo '<th>Rol</th>';
+                                    echo '<th>Fecha inicio</th>';
+                                    echo '<th>Sueldo</th>';
+                                    echo '<th>Informe</th>';
+                                    echo '<th colspan="2">Administrar</th>';
+                                echo '</tr>';
+                            echo '</thead>';
+                        echo '<tbody>';
+
+                        // Iterar sobre los resultados y mostrar empleados
+                        while ($row = mysqli_fetch_assoc($result)) {
                             echo '<tr>';
-                                echo '<th>Foto</th>';
-                                echo '<th>DNI</th>';
-                                echo '<th>Nombre</th>';
-                                echo '<th>Apellidos</th>';
-                                echo '<th>Email</th>';
-                                echo '<th>Rol</th>';
-                                echo '<th>Fecha inicio</th>';
-                                echo '<th>Sueldo</th>';
-                                echo '<th>Informe</th>';
-                                echo '<th colspan="2">Administrar</th>';
+                                echo '<td><img class="img-profile" src="' . $row['imagen'] . '" alt="Img"></td>';
+                                echo '<td>' . $row['dni'] . '</td>';
+                                echo '<td>' . $row['nombre'] . '</td>';
+                                echo '<td>' . $row['apellidos'] . '</td>';
+                                echo '<td>' . $row['email'] . '</td>';
+
+                                // Obtener nombre del rol
+                                $rol_id = $row['id_rol'];
+                                $rol_query = "SELECT nombre_rol FROM roles WHERE id_rol = $rol_id";
+                                $rol_result = mysqli_query($db, $rol_query);
+                                $rol_nombre = mysqli_fetch_assoc($rol_result)['nombre_rol'];
+
+                                echo '<td>' . $rol_nombre . '</td>';
+                                echo '<td>' . $row['fecha_inicio'] . '</td>';
+                                echo '<td>' . number_format($row['sueldo'], 0, ',','.') . '</td>';
+                                // Enlace para descargar el PDF
+                                // $pdf_dni = $row['dni'] . '.pdf'; // Nombre del archivo PDF
+                                // echo '<td><a href="../assets/pdf/ejemplo.pdf" class="btn-table btn-download" download="' . $pdf_dni . '"><img src="../assets/img/icons/download.svg" alt="Descargar"></a></td>';
+                                echo '<td><a href="../assets/pdf/ejemplo.pdf" class="btn-table btn-download" download><img src="../assets/img/icons/download.svg" alt="Descargar"></a></td>';
+                                echo '<td><a href="../actions/edit-employee.php?dni=' . $row['dni'] . '" class="btn-table btn-edit"><img src="../assets/img/icons/edit.svg" alt="Editar"></a></td>';
+                                echo '<td><a href="../actions/delete-employee.php?dni=' . $row['dni'] . '" class="btn-table btn-delete"><img src="../assets/img/icons/delete.svg" alt="Eliminar"></a></td>';
                             echo '</tr>';
-                        echo '</thead>';
-                    echo '<tbody>';
+                        }
 
-                    // Iterar sobre los resultados y mostrar empleados
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo '<tr>';
-                            echo '<td><img class="img-profile" src="' . $row['imagen'] . '" alt="Img"></td>';
-                            echo '<td>' . $row['dni'] . '</td>';
-                            echo '<td>' . $row['nombre'] . '</td>';
-                            echo '<td>' . $row['apellidos'] . '</td>';
-                            echo '<td>' . $row['email'] . '</td>';
-
-                            // Obtener nombre del rol
-                            $rol_id = $row['id_rol'];
-                            $rol_query = "SELECT nombre_rol FROM roles WHERE id_rol = $rol_id";
-                            $rol_result = mysqli_query($db, $rol_query);
-                            $rol_nombre = mysqli_fetch_assoc($rol_result)['nombre_rol'];
-
-                            echo '<td>' . $rol_nombre . '</td>';
-                            echo '<td>' . $row['fecha_inicio'] . '</td>';
-                            echo '<td>' . number_format($row['sueldo'], 0, ',','.') . '</td>';
-                            // Enlace para descargar el PDF
-                            // $pdf_dni = $row['dni'] . '.pdf'; // Nombre del archivo PDF
-                            // echo '<td><a href="../assets/pdf/ejemplo.pdf" class="btn-table btn-download" download="' . $pdf_dni . '"><img src="../assets/img/icons/download.svg" alt="Descargar"></a></td>';
-                            echo '<td><a href="../assets/pdf/ejemplo.pdf" class="btn-table btn-download" download><img src="../assets/img/icons/download.svg" alt="Descargar"></a></td>';
-                            echo '<td><a href="../actions/edit-employee.php?dni=' . $row['dni'] . '" class="btn-table btn-edit"><img src="../assets/img/icons/edit.svg" alt="Editar"></a></td>';
-                            echo '<td><a href="../actions/delete-employee.php?dni=' . $row['dni'] . '" class="btn-table btn-delete"><img src="../assets/img/icons/delete.svg" alt="Eliminar"></a></td>';
-                        echo '</tr>';
+                        echo '</tbody>';
+                        echo '</table>';
+                    } else {
+                        echo 'No se encontraron empleados.';
                     }
+                    ?>
 
-                    echo '</tbody>';
-                    echo '</table>';
-                } else {
-                    echo 'No se encontraron empleados.';
-                }
-                ?>
-
-            </table>
-        </div>
-    </main>
+                </table>
+            </div>
+        </main>
+    </div>
 
 <?php include_once("../includes/footer.php"); ?>
