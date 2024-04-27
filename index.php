3c1873ef01d7edc98dd2b6b9378d93dd2341dfac
Alejandro Vives <alejandrovt1996@gmail.com>
2024-04-27 11:04:38 +0200

    new styles

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="./assets/css/style-index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="login-container">
